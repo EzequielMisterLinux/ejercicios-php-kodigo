dea9c19451f9599736f30ce238396cda9162c32e
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2024-11-12 19:44:45 -0600

    traduciendo minimum password de c++ a php

--- minimum_password_strength.php
+++ minimum_password_strength.php
@@ -1,91 +1,56 @@
 <?php
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-
 /*
  * Complete the 'minimumNumber' function below.
  *
  * The function is expected to return an INTEGER.
  * The function accepts following parameters:
- *  1. INTEGER n
- *  2. STRING password
+ * 1. INTEGER n
+ * 2. STRING password
  */
 
-int minimumNumber(int n, string password) {
-    string numbers = "0123456789";
-    string lower_case = "abcdefghijklmnopqrstuvwxyz";
-    string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string special_characters = "!@#$%^&*()-+";
+function minimumNumber($n, $password) {
+    $numbers = "0123456789";
+    $lower_case = "abcdefghijklmnopqrstuvwxyz";
+    $upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $special_characters = "!@#$%^&*()-+";
     
-    bool has_digit = false;
-    bool has_lower = false;
-    bool has_upper = false;
-    bool has_special = false;
-
-    for (char c : password) {
-        if (isdigit(c)) has_digit = true;
-        else if (islower(c)) has_lower = true;
-        else if (isupper(c)) has_upper = true;
-        else if (special_characters.find(c) != string::npos) has_special = true;
+    $has_digit = false;
+    $has_lower = false;
+    $has_upper = false;
+    $has_special = false;
+    
+    // Revisar cada carácter de la contraseña
+    for ($i = 0; $i < strlen($password); $i++) {
+        $c = $password[$i];
+        if (ctype_digit($c)) {
+            $has_digit = true;
+        } elseif (ctype_lower($c)) {
+            $has_lower = true;
+        } elseif (ctype_upper($c)) {
+            $has_upper = true;
+        } elseif (strpos($special_characters, $c) !== false) {
+            $has_special = true;
+        }
     }
-
-    int missing_types = 0;
-    if (!has_digit) missing_types++;
-    if (!has_lower) missing_types++;
-    if (!has_upper) missing_types++;
-    if (!has_special) missing_types++;
-
-    int characters_needed = max(missing_types, 6 - n);
     
-    return characters_needed;
+    // Contar tipos faltantes
+    $missing_types = 0;
+    if (!$has_digit) $missing_types++;
+    if (!$has_lower) $missing_types++;
+    if (!$has_upper) $missing_types++;
+    if (!$has_special) $missing_types++;
+    
+    // Calcular caracteres necesarios
+    $characters_needed = max($missing_types, 6 - $n);
+    
+    return $characters_needed;
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
+// Leer la entrada
+$n = intval(trim(fgets(STDIN)));
+$password = trim(fgets(STDIN));
 
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    string password;
-    getline(cin, password);
-
-    int answer = minimumNumber(n, password);
-
-    fout << answer << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
+// Calcular y mostrar el resultado
+$answer = minimumNumber($n, $password);
+echo $answer . "\n";
 ?>
